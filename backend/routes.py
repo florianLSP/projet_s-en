9dfbda66616d0ce40db19575b2b6391ba0d5c97f
florian <florian.lesp@gmail.com>
2025-02-05 21:05:20 +0100

    refactor(back): merge POST and GET routes

--- backend/routes.py
+++ backend/routes.py
@@ -3,29 +3,28 @@ from models import Habits
 
 
 def register_routes(app, db):
-    @app.route("/habits", methods=["GET"])
-    def get_habits():
-        habits = []
-        for habit in Habits.query.all():
-            habits.append({"id": habit.hid, "name": habit.name})
-        return jsonify(habits)
+    @app.route("/habits", methods=["GET", "POST"])
+    def habits():
+        if request.method == "GET":
+            habits = []
+            for habit in Habits.query.all():
+                habits.append({"id": habit.hid, "name": habit.name})
+            return jsonify(habits)
+        elif request.method == "POST":
+            data = request.get_json()
+            if not data or "name" not in data:
+                return jsonify({"error": "le champ 'name' est requis"}), 400
 
-    @app.route("/add_habit", methods=["POST"])
-    def add_habits():
-        data = request.get_json()
-        if not data or "name" not in data:
-            return jsonify({"error": "le champ 'name' est requis"}), 400
+            new_habit = Habits(name=data["name"])
+            db.session.add(new_habit)
+            db.session.commit()
 
-        new_habit = Habits(name=data["name"])
-        db.session.add(new_habit)
-        db.session.commit()
-
-        return jsonify(
-            {
-                "message": "Habitude ajoutée avec succès",
-                "habit": {"id": new_habit.hid, "name": new_habit.name},
-            }
-        ), 201
+            return jsonify(
+                {
+                    "message": "Habitude ajoutée avec succès",
+                    "habit": {"id": new_habit.hid, "name": new_habit.name},
+                }
+            ), 201
 
     @app.route("/delete_habit/<id>", methods=["DELETE"])
     def delete_habit(id):
