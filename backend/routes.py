be76b19267789f24d5a1b26a100c361950ac04a0
florian <florian.lesp@gmail.com>
2025-02-05 15:24:35 +0100

    feat(back): add new delete route

--- backend/routes.py
+++ backend/routes.py
@@ -1,6 +1,5 @@
-from flask import render_template, request, jsonify
+from flask import request, jsonify
 from models import Habits
-from app import db
 
 
 def register_routes(app, db):
@@ -10,17 +9,27 @@ def register_routes(app, db):
         for habit in Habits.query.all():
             habits.append({"id": habit.hid, "name": habit.name})
         return jsonify(habits)
-    
 
     @app.route("/add_habit", methods=["POST"])
     def add_habits():
         data = request.get_json()
         if not data or "name" not in data:
             return jsonify({"error": "le champ 'name' est requis"}), 400
-        
+
         new_habit = Habits(name=data["name"])
         db.session.add(new_habit)
         db.session.commit()
 
-        return jsonify({"message": "Habitude ajoutée avec succès", "habit": {"id": new_habit.hid, "name": new_habit.name}}), 201
+        return jsonify(
+            {
+                "message": "Habitude ajoutée avec succès",
+                "habit": {"id": new_habit.hid, "name": new_habit.name},
+            }
+        ), 201
 
+    @app.route("/delete_habit/<id>", methods=["DELETE"])
+    def delete_habit(id):
+        db.session.delete(Habits.query.get(id))
+        db.session.commit()
+
+        return jsonify({"message": "L'habitude a été supprimée"}), 201
