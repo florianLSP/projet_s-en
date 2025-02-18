42b88794eff5a20e9fe021842839058b5fdc5eb4
florian <florian.lesp@gmail.com>
2025-02-18 21:03:54 +0100

    feat(front): create selectedHabit in the store

--- frontend/src/stores/habit.ts
+++ frontend/src/stores/habit.ts
@@ -5,8 +5,10 @@ import type { Habit } from '@/views/HomeView.vue'
 
 export const useHabitStore = defineStore('habit', () => {
   const habits: Ref<Array<Habit>> = ref([])
+  const selectedHabit: Ref<Habit | undefined> = ref()
 
   return {
     habits,
+    selectedHabit,
   }
 })
