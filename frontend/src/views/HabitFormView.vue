a3d92b8954790ee3988971abf7feabab349e8910
florian <florian.lesp@gmail.com>
2025-02-18 15:29:53 +0100

    feat(front): add to the form, description field

--- frontend/src/views/HabitFormView.vue
+++ frontend/src/views/HabitFormView.vue
@@ -5,6 +5,7 @@ import { useHabitStore } from '@/stores/habit'
 import SideBar from '@/components/SideBar.vue'
 
 const habitName = ref('')
+const habitDescription = ref('')
 const habitStore = useHabitStore()
 
 async function submitHabit() {
@@ -16,12 +17,14 @@ async function submitHabit() {
   try {
     const response = await axios.post('http://127.0.0.1:5000/habits', {
       name: habitName.value,
+      description: habitDescription.value,
     })
 
     const newHabit = response.data.habit
     habitStore.habit.push({
       id: newHabit.id,
       name: newHabit.name,
+      description: newHabit.description,
       creationDate: newHabit.creationDate,
     })
 
@@ -41,22 +44,34 @@ async function submitHabit() {
     <div class="flex-1 p-5 ml-64">
       <div class="flex pt-5 w-full justify-center">
         <form>
-          <div class="flex w-full items-center space-x-2">
-            <label for="name" class="text-gray-900 dark:text-white whitespace-nowrap"
-              >Nom de l'habitude:</label
-            >
-            <input
-              v-model="habitName"
-              type="text"
-              name="name"
-              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            />
+          <div class="flex flex-col">
+            <div class="flex w-full items-center space-x-2">
+              <label for="name" class="text-gray-900 dark:text-white whitespace-nowrap"
+                >Nom de l'habitude:</label
+              >
+              <input
+                v-model="habitName"
+                type="text"
+                name="name"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              />
+            </div>
+            <div class="flex w-full items-center space-x-2">
+              <label for="name" class="text-gray-900 dark:text-white whitespace-nowrap"
+                >Description de l'habitude:</label
+              >
+              <input
+                v-model="habitDescription"
+                type="text"
+                name="Description"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              />
+            </div>
           </div>
 
           <div class="flex justify-end">
             <router-link :to="{ name: 'home' }">
               <input
-                @click="submitHabit()"
                 type="button"
                 value="Retour"
                 class="bg-sen-light_red mr-1.5 inline-flex items-center gap-x-1.5 rounded-md px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-sen-dark_red cursor-pointer mt-5"
