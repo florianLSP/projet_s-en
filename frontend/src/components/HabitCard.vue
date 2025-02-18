758e45537f16cffa1ac6fa4de90f4d437febd4e9
florian <florian.lesp@gmail.com>
2025-02-18 11:26:07 +0100

    feat(front): create HabitCard component

---
+++ frontend/src/components/HabitCard.vue
@@ -0,0 +1,87 @@
+<script setup lang="ts">
+import { ref, onMounted, onBeforeUnmount } from 'vue'
+import { EllipsisHorizontalIcon } from '@heroicons/vue/24/solid'
+
+const cardMenu = ref(false)
+
+const props = defineProps({
+  name: String,
+  creationDate: String,
+})
+
+function toggleMenu() {
+  cardMenu.value = !cardMenu.value
+}
+
+function handleClickOutside(event: Event) {
+  const target = event.target as HTMLElement
+  if (!target.closest('.dropdown-wrapper')) {
+    cardMenu.value = false
+  }
+}
+
+onMounted(() => {
+  window.addEventListener('click', handleClickOutside)
+})
+
+onBeforeUnmount(() => {
+  window.removeEventListener('click', handleClickOutside)
+})
+</script>
+
+<template>
+  <div
+    class="w-80 max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700"
+  >
+    <div class="flex justify-end px-4 pt-4 relative dropdown-wrapper">
+      <button
+        @click.stop="toggleMenu"
+        class="inline-block text-red-500 dark:text-red-400 hover:bg-red-100 dark:hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-1.5"
+        type="button"
+      >
+        <span class="sr-only">Open dropdown</span>
+        <EllipsisHorizontalIcon class="w-5 h-5" />
+      </button>
+
+      <transition
+        enter-active-class="transition-opacity duration-300"
+        enter-from-class="opacity-0"
+        enter-to-class="opacity-100"
+        leave-active-class="transition-opacity duration-300"
+        leave-from-class="opacity-100"
+        leave-to-class="opacity-0"
+      >
+        <div
+          v-show="cardMenu"
+          class="z-10 absolute right-0 mt-2 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 dark:bg-gray-700"
+        >
+          <ul class="py-2" aria-labelledby="dropdownButton">
+            <li>
+              <a
+                href="#"
+                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                >Edit</a
+              >
+            </li>
+            <li>
+              <a
+                href="#"
+                class="block px-4 py-2 text-sm text-red-600 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                >Delete</a
+              >
+            </li>
+          </ul>
+        </div>
+      </transition>
+    </div>
+
+    <div class="flex flex-col items-center pb-10">
+      <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">
+        {{ props.name }}
+      </h5>
+      <span class="text-sm text-gray-500 dark:text-gray-400">
+        {{ props.creationDate }}
+      </span>
+    </div>
+  </div>
+</template>
