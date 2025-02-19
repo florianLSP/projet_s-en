9eace1df91bcba7aea429b77235aae0d7547a8c4
florian <florian.lesp@gmail.com>
2025-02-19 18:43:29 +0100

    feat(back): create habit_logs table

--- backend/models.py
+++ backend/models.py
@@ -9,6 +9,19 @@ class Habit(db.Model):
     name = db.Column(db.Text, nullable=False)
     description = db.Column(db.Text)
     creation_date = db.Column(db.DateTime, default=datetime.datetime.now)
+    habit_logs = db.relationship(
+        "HabitLog", back_populates="habit", cascade="all, delete-orphan"
+    )
 
     def __repr__(self):
         return f"Habit with name {self.name}"
+
+
+class HabitLog(db.Model):
+    __tablename__ = "habit_logs"
+
+    habit_log_id = db.Column(db.Integer, primary_key=True)
+    habit_id = db.Column(db.Integer, db.ForeignKey("habits.habit_id"), nullable=False)
+    date = db.Column(db.DateTime)
+
+    habit = db.relationship("Habit", back_populates="habit_logs")
