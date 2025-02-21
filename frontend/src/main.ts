59de7b7940eeb18de55a9f22c68ed35bb70861e9
florian <florian.lesp@gmail.com>
2025-02-21 22:42:36 +0100

    feat(front): add date picker to the form

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,10 +4,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
+import VueDatePicker from '@vuepic/vue-datepicker'
+import '@vuepic/vue-datepicker/dist/main.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.component('VueDatePicker', VueDatePicker)
 
 app.mount('#app')
--- frontend/src/views/HabitFormView.vue
+++ frontend/src/views/HabitFormView.vue
@@ -3,10 +3,16 @@ import { ref } from 'vue'
 import axios from 'axios'
 import { useHabitStore } from '@/stores/habit'
 import SideBar from '@/components/SideBar.vue'
+import VueDatePicker from '@vuepic/vue-datepicker'
+import '@vuepic/vue-datepicker/dist/main.css'
+import type { HabitLog } from './HomeView.vue'
+import router from '@/router'
 
 const habitName = ref('')
 const habitDescription = ref('')
 const habitStore = useHabitStore()
+const isNewHabit = ref('true')
+const date = ref()
 
 async function submitHabit() {
   if (!habitName.value) {
@@ -15,9 +21,11 @@ async function submitHabit() {
   }
 
   try {
+    const formattedDates = date.value ? date.value.map((d: HabitLog) => ({ date: d })) : []
     const response = await axios.post('http://127.0.0.1:5000/habits', {
       name: habitName.value,
       description: habitDescription.value,
+      habitLogs: formattedDates,
     })
 
     const newHabit = response.data.habit
@@ -26,11 +34,13 @@ async function submitHabit() {
       name: newHabit.name,
       description: newHabit.description,
       creationDate: newHabit.creationDate,
+      habitLogs: newHabit.habit_logs,
     })
 
     console.log('Réponse du serveur:', response.data)
 
     habitName.value = ''
+    router.push({ name: 'home' })
   } catch (error) {
     console.error("Erreur lors de l'ajout de l'habitude:", error)
     alert('Une erreur est survenue.')
@@ -41,50 +51,84 @@ async function submitHabit() {
 <template>
   <div class="flex dark:bg-gray-600 h-screen">
     <SideBar />
-    <div class="flex-1 p-5 ml-64">
-      <div class="flex pt-5 w-full justify-center">
-        <form>
-          <div class="flex flex-col">
-            <div class="flex w-full items-center space-x-2">
-              <label for="name" class="text-gray-900 dark:text-white whitespace-nowrap"
-                >Nom de l'habitude:</label
-              >
+    <div class="flex-1 p-8 ml-64">
+      <div class="flex w-full justify-center">
+        <form class="bg-white dark:bg-gray-700 p-6 rounded-lg shadow-lg w-full max-w-lg space-y-6">
+          <h2 class="text-lg font-semibold text-gray-900 dark:text-white text-center">
+            Nouvelle habitude
+          </h2>
+
+          <div class="grid gap-4">
+            <div class="flex flex-col">
+              <label for="name" class="text-gray-900 dark:text-white">Nom de l'habitude :</label>
               <input
                 v-model="habitName"
                 type="text"
                 name="name"
-                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                class="mt-1 p-2 border focus:ring-0 rounded-lg focus:ring-sen-gray focus:border-sen-gray bg-gray-50 dark:bg-gray-700 dark:border-gray-600 dark:text-white outline-none"
               />
             </div>
-            <div class="flex w-full items-center space-x-2">
-              <label for="name" class="text-gray-900 dark:text-white whitespace-nowrap"
-                >Description de l'habitude:</label
-              >
+
+            <div class="flex flex-col">
+              <label for="description" class="text-gray-900 dark:text-white">Description :</label>
               <input
                 v-model="habitDescription"
                 type="text"
-                name="Description"
-                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                name="description"
+                class="mt-1 p-2 border rounded-lg focus:ring-sen-gray focus:border-sen-gray bg-gray-50 dark:bg-gray-700 dark:border-gray-600 dark:text-white outline-none"
               />
+            </div>
+
+            <div class="flex items-center space-x-3">
+              <input
+                checked
+                type="radio"
+                value="true"
+                name="habit-start"
+                v-model="isNewHabit"
+                class="w-4 h-4 text-blue-600 focus:ring-blue-500 dark:bg-gray-700 dark:border-gray-600"
+              />
+              <label for="today" class="text-gray-900 dark:text-gray-300"
+                >Commence aujourd'hui</label
+              >
+            </div>
+            <div class="flex items-center space-x-3">
+              <input
+                type="radio"
+                value="false"
+                name="habit-start"
+                v-model="isNewHabit"
+                class="w-4 h-4 text-blue-600 focus:ring-blue-500 dark:bg-gray-700 dark:border-gray-600"
+              />
+              <label for="already-started" class="text-gray-900 dark:text-gray-300"
+                >A déjà commencé</label
+              >
+            </div>
+
+            <div v-if="isNewHabit == 'false'">
+              <label for="name" class="text-gray-900 dark:text-white"
+                >Sélectionner une ou plusieurs dates :</label
+              >
+              <VueDatePicker v-model="date" multi-dates :enable-time-picker="false" class="mt-1" />
             </div>
           </div>
 
-          <div class="flex justify-end">
+          <div class="flex justify-end space-x-3">
             <router-link :to="{ name: 'home' }">
-              <input
+              <button
                 type="button"
-                value="Retour"
-                class="bg-sen-light_red mr-1.5 inline-flex items-center gap-x-1.5 rounded-md px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-sen-dark_red cursor-pointer mt-5"
-              />
+                class="bg-sen-light_red text-white px-4 py-2 rounded-lg shadow-md hover:bg-sen-dark_red transition"
+              >
+                Retour
+              </button>
             </router-link>
-            <router-link :to="{ name: 'home' }">
-              <input
-                @click="submitHabit()"
-                type="submit"
-                value="Valider"
-                class="bg-sen-light_green ml-1.5 inline-flex items-center gap-x-1.5 rounded-md px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-sen-dark_green cursor-pointer mt-5"
-              />
-            </router-link>
+            <button
+              @click.prevent="submitHabit"
+              type="submit"
+              class="bg-sen-light_green text-white px-4 py-2 rounded-lg shadow-md hover:bg-sen-dark_green transition"
+            >
+              Valider
+            </button>
           </div>
         </form>
       </div>
