90edba4dc867f6a7a4359b79dac67f371d220f9d
florian <florian.lesp@gmail.com>
2025-02-22 20:43:03 +0100

    refactor(back): factorization of POST and get

--- backend/routes.py
+++ backend/routes.py
@@ -7,61 +7,10 @@ def register_routes(app, db):
     @app.route("/habits", methods=["GET", "POST"])
     def habits():
         if request.method == "GET":
-            habits = Habit.query.all()
-            habits = []
-            for habit in Habit.query.all():
-                habits.append(
-                    {
-                        "id": habit.habit_id,
-                        "name": habit.name,
-                        "description": habit.description,
-                        "creationDate": habit.creation_date,
-                        "logs": [
-                            {"id": log.habit_log_id, "date": log.date}
-                            for log in habit.habit_logs
-                        ],
-                    }
-                )
-            return jsonify(habits)
+            return get_habits()
 
         elif request.method == "POST":
-            data = request.get_json()
-            if not data or "name" not in data:
-                return jsonify({"error": "le champ 'name' est requis"}), 400
-
-            new_habit = Habit(
-                name=data["name"],
-                description=data["description"],
-            )
-            db.session.add(new_habit)
-            db.session.commit()
-
-            if len(data["habitLogs"]) > 0:
-                for habit_log in data["habitLogs"]:
-                    date_str = habit_log["date"]
-                    date_str = date_str.replace("Z", "")
-                    log = HabitLog(
-                        habit_id=new_habit.habit_id,
-                        date=datetime.fromisoformat(date_str),
-                    )
-                    db.session.add(log)
-                db.session.commit()
-
-            return jsonify(
-                {
-                    "message": "Habitude ajoutée avec succès",
-                    "habit": {
-                        "id": new_habit.habit_id,
-                        "name": new_habit.name,
-                        "description": new_habit.description,
-                        "creationDate": new_habit.creation_date,
-                        "habit_logs": [
-                            {"id": log.habit_log_id, "date": log.date}
-                            for log in new_habit.habit_logs
-                        ],
-                    },
-                }
-            ), 201
+            return post_habit()
 
     @app.route("/habit/<id>", methods=["DELETE"])
     def delete_habit(id):
@@ -88,3 +37,60 @@ def register_routes(app, db):
                 return jsonify({"error": "Habitude introuvable"}), 404
 
             return jsonify(habit)
+
+    def get_habits():
+        habits = Habit.query.all()
+        habits = []
+        for habit in Habit.query.all():
+            habits.append(
+                {
+                    "id": habit.habit_id,
+                    "name": habit.name,
+                    "description": habit.description,
+                    "creationDate": habit.creation_date,
+                    "logs": [
+                        {"id": log.habit_log_id, "date": log.date}
+                        for log in habit.habit_logs
+                    ],
+                }
+            )
+        return jsonify(habits)
+
+    def post_habit():
+        data = request.get_json()
+        if not data or "name" not in data:
+            return jsonify({"error": "le champ 'name' est requis"}), 400
+
+        new_habit = Habit(
+            name=data["name"],
+            description=data["description"],
+        )
+        db.session.add(new_habit)
+        db.session.commit()
+
+        if len(data["habitLogs"]) > 0:
+            for habit_log in data["habitLogs"]:
+                date_str = habit_log["date"]
+                date_str = date_str.replace("Z", "")
+                log = HabitLog(
+                    habit_id=new_habit.habit_id,
+                    date=datetime.fromisoformat(date_str),
+                )
+                db.session.add(log)
+                db.session.commit()
+
+            return jsonify(
+                {
+                    "message": "Habitude ajoutée avec succès",
+                    "habit": {
+                        "id": new_habit.habit_id,
+                        "name": new_habit.name,
+                        "description": new_habit.description,
+                        "creationDate": new_habit.creation_date,
+                        "habit_logs": [
+                            {"id": log.habit_log_id, "date": log.date}
+                            for log in new_habit.habit_logs
+                        ],
+                    },
+                }
+            ), 201
